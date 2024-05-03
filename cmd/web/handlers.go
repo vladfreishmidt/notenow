ed8d427a323b8379a34e84114b114d86aa775b7a
vladfreishmidt <vladfreishmidt.webdevelopment@gmail.com>
2024-05-03 15:53:40 +0300

    Feature: Implement Insert Get Latest note model methods

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"html/template"
 	"net/http"
 	"strconv"
+
+	"github.com/vladfreishmidt/notenow/internal/models"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -13,34 +15,34 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	files := []string{
-		"./ui/html/base.gohtml",
-		"./ui/html/partials/nav.gohtml",
-		"./ui/html/pages/home.gohtml",
-	}
-
-	// parse html template
-	ts, err := template.ParseFiles(files...)
+	notes, err := app.notes.Latest()
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
-	// execute html template
-	err = ts.ExecuteTemplate(w, "base", nil)
-	if err != nil {
-		app.serverError(w, err)
-		return
-	}
-}
 
-func (app *application) noteView(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	if err != nil || id < 1 {
-		app.notFound(w)
-		return
+	for _, note := range notes {
+		fmt.Fprintf(w, "%+v\n", note)
 	}
 
-	fmt.Fprintf(w, "Display a specific note with ID %d...", id)
+	// files := []string{
+	// 	"./ui/html/base.gohtml",
+	// 	"./ui/html/partials/nav.gohtml",
+	// 	"./ui/html/pages/home.gohtml",
+	// }
+
+	// // parse html template
+	// ts, err := template.ParseFiles(files...)
+	// if err != nil {
+	// 	app.serverError(w, err)
+	// 	return
+	// }
+	// // execute html template
+	// err = ts.ExecuteTemplate(w, "base", nil)
+	// if err != nil {
+	// 	app.serverError(w, err)
+	// 	return
+	// }
 }
 
 func (app *application) noteCreate(w http.ResponseWriter, r *http.Request) {
@@ -50,5 +52,36 @@ func (app *application) noteCreate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte("Create a new note.."))
+	title := "Super cool note"
+	content := "Hey! This is my super cool note content! Wow!"
+	expires := 7
+
+	id, err := app.notes.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/note/view?id=%d", id), http.StatusSeeOther)
+}
+
+func (app *application) noteView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		app.notFound(w)
+		return
+	}
+
+	note, err := app.notes.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+
+		return
+	}
+
+	fmt.Fprintf(w, "%+v", note)
 }
--- internal/models/notes.go
+++ internal/models/notes.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 )
 
@@ -18,13 +19,69 @@ type NoteModel struct {
 }
 
 func (m *NoteModel) Insert(title, content string, expires int) (int, error) {
-	return 0, nil
+	stmt := `INSERT INTO notes (title, content, created, expires)
+	VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+
+	result, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
 }
 
 func (m *NoteModel) Get(id int) (*Note, error) {
-	return nil, nil
+	stmt := `SELECT id, title, content, created, expires FROM notes
+	WHERE expires > UTC_TIMESTAMP() AND id = ?`
+
+	row := m.DB.QueryRow(stmt, id)
+
+	n := &Note{}
+
+	err := row.Scan(&n.ID, &n.Title, &n.Content, &n.Created, &n.Expires)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrNoRecord
+		} else {
+			return nil, err
+		}
+	}
+
+	return n, nil
 }
 
 func (m *NoteModel) Latest() ([]*Note, error) {
-	return nil, nil
+	stmt := `SELECT id, title, content, created, expires FROM notes
+	WHERE expires > UTC_TIMESTAMP() ORDER BY id DESC LIMIT 10`
+
+	rows, err := m.DB.Query(stmt)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	notes := []*Note{}
+
+	for rows.Next() {
+		n := &Note{}
+
+		err = rows.Scan(&n.ID, &n.Title, &n.Content, &n.Created, &n.Expires)
+		if err != nil {
+			return nil, err
+		}
+
+		notes = append(notes, n)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return notes, nil
 }
